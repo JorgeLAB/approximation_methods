6fb8826fc2174779af42455dad7fc1be94883981
Jorge Borges <jorgetrabalho2016@gmail.com>
2025-11-30 15:57:33 -0300

    update module

--- src/approximation_method/__init__.py
+++ src/approximation_method/__init__.py
@@ -1,5 +1,3 @@
 from __future__ import annotations
 
-from .least_squares_simple_linear import LeastSquaresSimpleLinear
-
-__all__ = ['LeastSquaresSimpleLinear']
+from .least_squares_simple_linear import LeastSquaresSimpleLinear as LeastSquaresSimpleLinear
