dd8d712a6b8ffa61e97c30781c3522827d9f9021
Jorge Borges <jorgetrabalho2016@gmail.com>
2025-11-30 16:18:15 -0300

    update version file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='approxmethods',
-    version='0.1.2',
+    version='0.1.0',
     packages=find_packages("src"),
     package_dir={'': 'src'},
     author='Jorge Borges, Eduardo Freitas, Thaina Barcellos, Josué Fonseca, Lucas Figueiredo',
