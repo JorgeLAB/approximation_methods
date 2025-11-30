117483acd16a8ee24b36095d9819f9c431d26e11
Jorge Borges <jorgetrabalho2016@gmail.com>
2025-11-30 16:17:53 -0300

    update name file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='approximation_methods',
+    name='approxmethods',
     version='0.1.2',
     packages=find_packages("src"),
     package_dir={'': 'src'},
